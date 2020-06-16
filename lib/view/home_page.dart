dc9c4bc5311f29bc3fddc61b89663f658ad8f23d
Hammad Khan <hammadkhn30@gmail.com>
2020-06-16 16:27:11 +0530

    Restarted the project

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Seek Recipes"),
+      ),
+    );
   }
 }
