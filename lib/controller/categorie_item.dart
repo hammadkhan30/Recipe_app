388eccea8fd8a8a1aecd647619e54790857b361d
Hammad Khan <hammadkhn30@gmail.com>
2020-06-17 17:32:46 +0530

    added the Categorie_item.dart

---
+++ lib/controller/categorie_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategorieItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategorieItem(this.title,this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
